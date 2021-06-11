a07d17c814744dc9a7deeaa2484c539b54b9ff50
Susan Joshi <susanbdrjoshi1@gmail.com>
2021-06-11 10:59:08 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 var doubleRunPowerRequired = {
-  one: {
+  zero_five: {
     six: 0.042,
     eight: 0.076,
     ten: 0.114,
     twelve: 0.18
   },
 
-  two: {
+  ten_fifteen: {
     six: 0.062,
     eight: 0.113,
     ten: 0.17,
     twelve: 0.18,
   },
 
-  three: 
+  twenty_thirty: 
   {
     six: 0.085,
     eight: 0.167 ,
@@ -21,14 +21,14 @@ var doubleRunPowerRequired = {
     twelve: 0.397
   },
 
-  four: {
+  thirtyfive_fortyfive: {
     six: 0.12,
     eight: 0.222,
     ten: 0.333,
     twelve: 0.524
   },
 
-  five: {
+  fifty_sixty: {
     six: 0.15,
     eight: 0.27,
     ten: 0.405,
@@ -36,105 +36,170 @@ var doubleRunPowerRequired = {
   },
 };
 
-var findHp = function (obj, a, d) {
-  let value = CovertToString(a);
-  for(key in obj)
-  {
-    if(key === value)
-    {
-      for(k in obj[key])
-      {
-        if(k === d)
-        {
-          console.log(obj[key][k]);
-          test(obj[key][k]);
-        }
-        
-
-      }
-    }
-  }
-};
-
-const test = (obj) => {
-
-let test = (66 + 11) * obj;
-console.log(test, " Test");
-  return test;
-}
-
 const motorHpList = [3, 5, 7.5, 10, 15, 20, 25, 30, 40, 50, 60];
 
-const SetHp = () =>
+const FindHp = () =>
 {
-  const calculatedHp = 24.642;
-  console.log(calculatedHp, "FindHp");
   let hp;
   const aboveNine = 9;
   const belowSevenPointFive = 7.5;
-  let matchingStandardHp = parseInt(FindMatch(calculatedHp));
+  const calculatedHp = SetCalculatedHp();
+  let matchingStandardHp = parseInt(FindMatchingHp(calculatedHp));
   for(let i = matchingStandardHp; i < motorHpList.length; i++)
     {
 	  if(calculatedHp > aboveNine && ((motorHpList[i] - calculatedHp) <= 1))
     {
       hp = motorHpList[i + 1];
-      console.log("This is the final result: ", hp);
       return hp;
     }
 	   	
 	  else if(calculatedHp < belowSevenPointFive && ((motorHpList[i] - calculatedHp) <= 0.5))
     {
       hp = motorHpList[i + 1];
-      console.log("This is the final result: ", hp);
       return hp;
     }
 	   
     else
     {
       hp = motorHpList[i];
-      console.log("This is the final result: ", hp);
       return hp;
     }
       
     }
 	 
-    //console.log("This is the final result: ", hp);
   return hp;
 }
 
-const FindMatch = calculatedHp => {
-for(let hp in motorHpList)
-  if(calculatedHp <= motorHpList[hp])
-    return hp;
+function SetHp()
+{
+  document.getElementById('requiredHp').innerHTML = FindHp();
+}
+
+const FindMatchingHp = calculatedHp => {
+  for(let hp in motorHpList)
+    if(calculatedHp <= motorHpList[hp])
+	 return hp;
  
   return 0;
 }
 
-function CovertToString(angle)
+const FindCoefficient = (obj, inclineAngle, capacity) => {
+  const angle = CovertToString(inclineAngle);
+  for(let item in obj)
+  {
+    if(item === angle)
+    {
+      for(let key in obj[item])
+        if(key === capacity)
+	   	return obj[item][key];
+    }
+  }
+  
+  return 0;
+};
+
+const SetCalculatedHp = () => 
 {
-  let convertToString
+  const straightConveyorLength = parseInt(document.getElementById('straightConveyorLength').value);
+  console.log("Straight Conveyor Length: ", straightConveyorLength);
+  const futureExpansionLength = parseInt(document.getElementById('futureExpansionAdditionalLength').value);
+  console.log("Future Expansion Length:",  futureExpansionLength);
+  const capacity = document.getElementById('capacitySelection').value;
+  const straightConveyorInclineAngle = document.getElementById('straightConveyorInclineAngle').value;
+  const commodityMultiplier = parseInt(document.getElementById('commoditySelection').value);
+  const coefficient = FindCoefficient(doubleRunPowerRequired, straightConveyorInclineAngle, capacity);
+  
+  const horizontalComponent = ((straightConveyorLength + futureExpansionLength) 
+						 * coefficient) * commodityMultiplier;	  
+  document.getElementById('standardHpCalculation').innerHTML ="((" + straightConveyorLength +
+  " + " + futureExpansionLength + ") * " + coefficient + ") * " + commodityMultiplier;
+  document.getElementById('commodity').innerHTML = commodityMultiplier;
+  document.getElementById('coefficient').innerHTML = coefficient;
+  document.getElementById('standardHp').innerHTML = horizontalComponent;
+  HighlightCoefficient(coefficient);
+
+  return horizontalComponent;
+}
+
+const HighlightCoefficient = coefficient => {
+  let hpTable = document.getElementById('hpTable').getElementsByTagName('tr');
+  for(let i = 0; i < hpTable.length; i++)
+  {
+    let tr = hpTable.item(i);
+    var tds = tr.getElementsByTagName('td');
+    for(let j = 0; j < tds.length; j++)
+    { 
+      var td = tds[j].innerHTML;
+      if(td == coefficient)
+      tds[j].style.color = "#00AA00";
+      else
+      tds[j].style.color = '';
+    }
+  }
+
+}
+
+const CovertToString = angle =>
+{
+  let convertToString;
   switch(angle)
   {
     case "0_5":
-    convertToString = "one";
+    convertToString = "zero_five";
     break;
     case "10_15":
-    convertToString = "two";
+    convertToString = "ten_fifteen";
     break;
     case "20_30":
-    convertToString = "three";
+    convertToString = "twenty_thirty";
     break;
     case "35_45":
-    convertToString = "four";
+    convertToString = "thirtyfive_fortyfive";
     break;
     case "50_60":
-    convertToString = "five";
+    convertToString = "fifty_sixty";
     break;
   }
 
   return convertToString;
 }
 
- findHp(doubleRunPowerRequired, "0_5", "six");
- //SetHp();
-//FindMatch(4.26);
+const PopulateStraightConveyorLength = () =>
+{
+  let select = document.getElementById("straightConveyorLength");
+  let counter = 20;
+  let start = 11;
+  
+  while(counter > 0)
+  {
+    let length = document.createElement('option');
+    length.innerHTML = start + " ft";
+    length.value = start;
+    select.appendChild(length);
+    start += 5;
+    counter--;
+  }
+}
+
+const PopulateFutureExpansionAdditionalLength = () =>
+{
+  let select = document.getElementById("futureExpansionAdditionalLength");
+  let counter = 20;
+  let start = 0;
+
+  while(counter > 0)
+  {
+    let length = document.createElement('option');
+    length.innerHTML = start + " ft";
+    length.value = start;
+    select.appendChild(length);
+    start += 5;
+    counter--;
+  }
+}
+ 
+ PopulateStraightConveyorLength();
+ PopulateFutureExpansionAdditionalLength();
+ SetCalculatedHp();
+ SetHp();
+ 
